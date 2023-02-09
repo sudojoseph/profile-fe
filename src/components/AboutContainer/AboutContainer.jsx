60e6d1f89a986e62bc8bd180e1117644d7e80849
sudojoseph <josephdavidson1989@gmail.com>
2023-02-09 15:14:14 +0200

    stop about scrolling

--- src/components/AboutContainer/AboutContainer.jsx
+++ src/components/AboutContainer/AboutContainer.jsx
@@ -5,10 +5,7 @@ const AboutContainer = () => {
   return (
     <>
     
-    <div className='split-content-container about-container'
-    data-scroll
-    data-scroll-direction="horizontal"
-    data-scroll-speed="-4">
+    <div className='split-content-container about-container'>
     <p className={'about-text'}
         >"Nice to meet you! I'm a passionate full stack developer with a love for programming that extends beyond my day job. With strong skills in both front and back end development, I approach each project with dedication and enthusiasm. Whether working on a complex web application or a simple website, I bring a level of passion and attention to detail that is sure to impress. If you're looking for a developer who is not only highly skilled but also really fun to work with, I'm your guy."</p>
       <div className='profile-picture-container'>
