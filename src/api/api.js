2d39c78224ebcabd46cdb817ee82e6305c217522
sudojoseph <josephdavidson1989@gmail.com>
2023-03-17 12:51:28 +0200

    test changes live

--- src/api/api.js
+++ src/api/api.js
@@ -11,16 +11,17 @@ export class Api {
 
     constructor() {
         this.baseUrl = process.env.REACT_APP_SERVER_URL;
-        this.jarvisServiceUrl = process.env.JARVES_SERVER_URL
+        this.jarvisServiceUrl = 'https://ancient-journey-07275.herokuapp.com'
     };
 
     askTravisQuestion = async(question) => {
         const answer = await axios.post(`${this.jarvisServiceUrl}`, { question: question }, config);
-        return answer.data;
+        console.log(answer);
+        return answer;
     };
 
     sendMessageToTelegramBot = async(message) => {
-        const responce = await axios.post(`${this.baseUrl}/telegram`, { message: message });
+        const responce = await axios.post(`${this.baseUrl}/telegram`, { question: message });
         return responce.data;
     };
 };
