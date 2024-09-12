21041e17e1ff8f9ded466edb7edffeca66ba9b02
sudojoseph <josephdavidson1989@gmail.com>
2024-09-12 23:16:57 +0300

    adding data points to social media links

--- src/components/SocialMediaLinks/SocialMediaLinks.jsx
+++ src/components/SocialMediaLinks/SocialMediaLinks.jsx
@@ -1,16 +1,18 @@
 import './SocialMediaLinks.css';
 import Resume from '../../documents/Joseph_Davidson_CV.pdf';
+import { Api } from '../../api/api';
 
 const SocialMediaLinks = () => {
+  const api = new Api;
   return (
     <ul className='socialMediaLinks'>
-        <a href="https://www.linkedin.com/in/joseph-davidson-a90490105/" target='_blank'>
+        <a onClick={api.sendMessageToTelegramBot('Visiter opened LinkedIn')} href="https://www.linkedin.com/in/joseph-davidson-a90490105/" target='_blank'>
             <li>LinkedIn</li>
         </a>
-        <a href="https://github.com/sudojoseph" target='_blank'>
+        <a onClick={api.sendMessageToTelegramBot('Visiter opened Github')} href="https://github.com/sudojoseph" target='_blank'>
             <li>GitHub</li>
         </a>
-        <a href={Resume} download="Joseph Davidson Resume" target='_blank'>
+        <a onClick={api.sendMessageToTelegramBot('Visiter downloaded resume')} href={Resume} download="Joseph Davidson Resume" target='_blank'>
             <li>CV</li>
         </a>
     </ul>
